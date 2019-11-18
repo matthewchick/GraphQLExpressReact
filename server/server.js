8774a9e131adaa3412c51a4eff03f96b849cb152
Matthew Chick <c.chick@unsw.edu.au>
2019-11-19 10:28:19 +1100

    Connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -4,17 +4,26 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
-
+// to use process.env
+require('dotenv').config();
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb+srv://chikmatthew:!@#123QWE@nodeecommerce-2qbaf.mongodb.net/test?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://chikmatthew:Password1@nodeecommerce-2qbaf.mongodb.net/test?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+//mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+.then(()=> console.log('DB Connected'));
+
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
