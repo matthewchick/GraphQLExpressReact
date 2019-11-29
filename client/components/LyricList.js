93af019e673e3fd3ed114e3bc05b430be9b56f25
Matthew Chick <c.chick@unsw.edu.au>
2019-11-29 13:18:34 +1100

    add like mutation but have a bug which cannot show

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 class LyricList extends Component {
   onLike(id) {
-    console.log(id);
+    // console.log(id);
+    this.props.mutate({
+      variables: { id: id }
+    })
   }
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key = {id} className="collection-item">
           {content}
           <i className="material-icons"
             onClick={() => this.onLike(id)}
           >thumb_up</i>
+          {likes}
         </li>
       );
     });
@@ -25,4 +30,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
